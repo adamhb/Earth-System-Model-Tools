c9f917cbdc74c00cf60fb89b749634fa145579c4
Rachel Ward <rachel0578@gmail.com>
2021-10-01 16:09:41 -0700

    RL for canopy species, RL for canopy sp + herbivory

--- benchmarking/RL_BCI.R
+++ benchmarking/RL_BCI.R
@@ -1,15 +1,13 @@
-<<<<<<< HEAD
+
+library(tidyverse)
 library(lubridate)
-library(tidyverse)
-
-
-data_path <- "~/Desktop/NGEE-Tropics/BCI_data/"
-=======
-library(tidyverse)
 source('utils/figure_formatting_esm_tools.R')
+source('benchmarking/RL_functions.R')
+
+##############################Ecosystem level R/L , all species###########################
 
 data_path <- "data/"
->>>>>>> c953388409d994fe28c7416a383ae2deb616a893
+
 BCI_mo_RL <- read_csv(paste(data_path,"BCI_mo_RL.csv", sep = ""))
 
 BCI_mo_RL %>% 
@@ -21,9 +19,97 @@ BCI_mo_RL %>%
   ggplot() +
   geom_boxplot(aes(x = "", 
                    y = RL))+
-<<<<<<< HEAD
-  xlab("BCI R/L (all species)")
-=======
   xlab("BCI R/L (all species)") +
   adams_theme
->>>>>>> c953388409d994fe28c7416a383ae2deb616a893
+
+############################## Canopy sp  R/L##############################################
+
+
+##load data
+canopy_sp <- read_csv(paste(data_path, "canopy_sp_bci.csv", sep = "")) %>% 
+  rename(sp = `unique(bcifull_clean$sp)`)
+
+bci_traits <- read_csv("~/Desktop/NGEE-Tropics/BCI_data/BCITRAITS_20101220_updated3.21.19.csv") 
+
+
+trap_data <- read_csv("~/Desktop/NGEE-Tropics/BCI_data/Hanbury_Brown_50ha_rawdata_20190404.csv") %>% 
+  mutate(part = as_factor(part)) %>% 
+  mutate(sp = tolower(sp6)) %>% 
+  mutate(Date = as.Date(fecha, format = "%Y%m%d")) %>% 
+  mutate(Year = lubridate::year(Date)) %>% 
+  filter(sp %in% canopy_sp$sp) %>% # filter for only canopy sp
+  filter(Year>2013 & Year < 2019) ## full years of data
+
+fruit_mass_df <- bci_traits %>% 
+  mutate(sp = tolower(`SP$`)) %>% 
+  dplyr::select(sp, FRUIT_DRY, DSPR_DRY) %>%
+  mutate(fruit_mass  = case_when(
+    !is.na(FRUIT_DRY) ~ FRUIT_DRY, 
+    is.na(FRUIT_DRY)  ~ DSPR_DRY,
+  ))
+
+##designate part codes for reproductive and leaf material 
+repro_parts <- c(0:10) #part 10 is not in metadtaa seed rain file but from pers. communication with Joe Wright, represents damaged fruit
+leaf_parts <- 11
+#################
+
+##helper function to use with purr library functions 
+purr_fun <- function(yrdata){
+  yrdata$mass_m2_total
+} 
+
+## RL for canopy species with no herbivory correction 
+R_yr <- 
+  trap_data %>% 
+  group_by(Year) %>%
+  nest() %>% 
+  mutate(R_yr = map(data, get_part_mass_total, parts = repro_parts)) %>% 
+  transmute(Year, Rmass_m2 = map_dbl(R_yr, purr_fun))
+
+
+L_yr <- 
+  trap_data %>% 
+  group_by(Year) %>% 
+  nest() %>% 
+  mutate(L_yr = map(data, get_part_mass_total, parts = leaf_parts)) %>% 
+  transmute(Year, Lmass_m2 = map_dbl(L_yr, purr_fun))
+
+
+## RL for canopy species with herbivory correction 
+R_yr_corr_temp<- 
+  trap_data %>% 
+  group_by(Year) %>%
+  nest() %>% 
+  mutate(R_yr_corr = map(data, get_repro_total_m2)) %>% 
+  transmute(Year, Rmass_m2_corr = map_dbl(R_yr_corr, purr_fun))
+
+L_yr_corr <- L_yr %>% 
+  mutate(Lmass_m2_corr = Lmass_m2*1.10)
+
+##source: https://daac.ornl.gov/NPP/guides/NPP_BRR.html
+## see brr_npp_r1.txt, estsimated ~ 50g/m2yr lost to insect herbivory, ~30 g/m2yr to vert. herbiv.
+## this is proprtional to ~ 10% of the observed litterfall flux
+
+
+## Join 
+R_yr_corr <- R_yr %>%
+  left_join(R_yr_corr_temp)
+
+RL_df <- R_yr_corr %>% 
+  left_join(L_yr_corr) %>% 
+  mutate(RL = Rmass_m2 / Lmass_m2 ) %>% 
+  mutate(RL_corr = Rmass_m2_corr / Lmass_m2_corr) %>% 
+  select(Year, RL, RL_corr) %>% 
+  pivot_longer(cols = c(RL, RL_corr), names_to = "Type", values_to = "RL") 
+
+
+## Plot
+RL_df %>% 
+  ggplot() +
+  geom_boxplot(aes(x = Type,
+                   y = RL)) +
+  adams_theme 
+
+
+
+
---
+++ benchmarking/RL_functions.R
@@ -0,0 +1,92 @@
+
+
+library(tidyverse)
+#############################################Functions for trap data 
+
+
+
+####################get mass of designated litterfall part codes
+
+get_part_mass_total <- function(data, parts, trap_size_m2 = 0.5, by_sp = F){
+  #data is raw, in form sp, fecha, census, trap, part, quantity, mass
+  #part codes is a vector of integerts corresponding to codes recorded in the BCI Seed Rain Metadata file
+  #by_sp will return results grouped by sp if TRUE, else will return total mass
+  n_traps <- n_distinct(data$trap)
+  trap_size <- trap_size_m2
+  
+  if(by_sp == T){
+    data %>% 
+      filter(part %in% parts) %>% 
+      group_by(sp, part) %>% 
+      summarise(mass_sum_sp = sum(mass))%>% 
+      mutate(mass_m2_sp = mass_sum_sp / (n_traps*trap_size))
+  }else(
+    data %>% 
+      filter(part %in% parts) %>% 
+      group_by(sp, part) %>% 
+      summarise(mass_sum_sp = sum(mass))%>% 
+      mutate(mass_m2_sp = mass_sum_sp / (n_traps*trap_size)) %>% 
+      ungroup() %>%
+      summarise(mass_m2_total = sum(mass_m2_sp)))
+}
+
+
+#####################get mature fruit equivalent mass from relevant part codes 
+get_mf_eq_mass <- function (data, parts = c(3, 4, 7, 10), fruit_mass = fruit_mass_df) {
+  
+  #data cols are: sp, fecha, census, trap, part, quantity, mass
+  #part codes are as in METADATA BCI SEED RAIN, 
+  #fruit_mass_df cols are: "sp"  = sp6 code, "FRUIT_DRY" = dry mature fruit mass 
+  #default parts that represent mature fruit equivalents:
+  #3:capsules, 4:fragments,7:fruit w insect emergence holes, 10: damaged fruit
+  # see email with Joe Wright April 3 2021; 
+  # fragments should in fact be multiplied by with sp specific ratios, conservative estimate is 1:1 ratio
+  
+  mf_eq <- data %>% 
+    filter(part %in% parts) %>% 
+    group_by(sp, part) %>% 
+    summarise(mf_equiv = sum(quantity)) %>% 
+    ungroup() %>% 
+    left_join(fruit_mass, by = "sp") %>%
+    rowwise() %>% 
+    mutate(mf_eq = mf_equiv * fruit_mass) %>% 
+    dplyr::select(sp, part, mf_eq)
+  return(mf_eq)
+  
+}
+
+################get total reproductive mass total, including correction from herbivory 
+
+get_repro_total_m2 <- function(data, fruit_mass = fruit_mass_df, trap_size_m2 = 0.5){
+  
+  #data cols are: sp, fecha, census, trap, part, quantity, mass
+  #part codes are as in METADATA BCI SEED RAIN, 
+  #fruit_mass_df cols are: "sp"  = sp6 code, "FRUIT_DRY" = dry mature fruit mass 
+  repro_mass <- get_part_mass_total(data, parts = c(0, 1, 2, 5, 6, 8, 9), trap_size_m2 = 0.5, by_sp = T) %>% 
+    group_by(sp) %>% 
+    summarise(repro_mass_sp = sum(mass_sum_sp), 
+              repro_mass_sp_m2 = sum(mass_m2_sp))
+  
+  mfe_mass<- get_mf_eq_mass(data, fruit_mass = fruit_mass_df) %>% ###### will use default parts, change if want to include capsules
+    group_by(sp) %>% 
+    summarise(mfe_mass = sum(mf_eq, na.rm = T))
+  
+  repro_total <-  repro_mass %>% 
+    select(sp, repro_mass_sp) %>% 
+    left_join(mfe_mass, by = "sp") %>% 
+    rowwise() %>%
+    mutate(repro_total = sum(repro_mass_sp, mfe_mass, na.rm = T)) %>% 
+    dplyr::select(sp, repro_total)
+  
+  n_traps <- n_distinct(data$trap)
+  trap_size <- trap_size_m2
+  
+  repro_total_m2 <- repro_total %>% 
+    mutate(repro_m2 = repro_total / (n_traps *trap_size)) %>% 
+    ungroup() %>% 
+    summarise(mass_m2_total = sum(repro_m2))
+  
+  return(as_tibble(repro_total_m2))
+}
+
+
