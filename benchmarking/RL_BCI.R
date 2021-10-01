c953388409d994fe28c7416a383ae2deb616a893
adamhb <ahanburybrown@gmail.com>
2021-10-01 12:26:55 -0700

    made canopy sp data set

---
+++ benchmarking/RL_BCI.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+source('utils/figure_formatting_esm_tools.R')
+
+data_path <- "data/"
+BCI_mo_RL <- read_csv(paste(data_path,"BCI_mo_RL.csv", sep = ""))
+
+BCI_mo_RL %>% 
+  group_by(Year) %>% 
+  summarise(Rm2yr = sum(Rm2), 
+            Lm2yr = sum(Lm2),
+            Sm2yr = sum(Sm2)) %>% 
+  mutate(RL = Rm2yr/Lm2yr) %>% 
+  ggplot() +
+  geom_boxplot(aes(x = "", 
+                   y = RL))+
+  xlab("BCI R/L (all species)") +
+  adams_theme
