8c7ba765f775d777189a8230cf5ad54c852ec3c5
Rachel Ward <rachel0578@gmail.com>
2021-10-07 22:11:25 -0700

    fixed problem wih grouping

--- benchmarking/RL_Luquillo.R
+++ benchmarking/RL_Luquillo.R
@@ -35,21 +35,25 @@ CTE_control <- CTE %>%
   mutate(Lgm2 = L / (nTraps_plot*trap_size_m2),
          Rgm2 = R / (nTraps_plot*trap_size_m2)) %>%# observations from each plot are sum of material from 10 traps
   mutate(Yr_mo = as.Date(paste(month(Date), "01", year(Date), sep = "/"), format = "%m/%d/%Y")) %>% # create Yr-mo for easy grouping
-  group_by(Yr_mo) %>% #get average monthly g/m2month across all block-plots
-  summarise(Lgm2mo = mean(Lgm2),
-            Rgm2mo = mean(Rgm2)) %>%
-  group_by(Year = year(Yr_mo)) %>% 
-  summarise(Lgm2yr = sum(Lgm2mo),
-            Rgm2yr = sum(Rgm2mo)) %>% 
+  group_by(Yr_mo, Block, Plot) %>% # monthly g/m2month for each plot within each block
+  summarise(Lgm2moplot = sum(Lgm2, na.rm = T),
+            Rgm2moplot = sum(Rgm2, na.rm = T)) %>%
+  group_by(Yr_mo) %>% # get monthly avg for all blocks 
+  summarise(Lgm2mo = mean(Lgm2moplot, na.rm = T),
+            Rgm2mo = mean(Rgm2moplot, na.rm = T)) %>% 
+  group_by(Year = year(Yr_mo)) %>%  # sum over year
+  summarise(Lgm2yr = sum(Lgm2mo, na.rm = T),
+            Rgm2yr = sum(Rgm2mo, na.rm = T)) %>% 
   mutate(RL = Rgm2yr/Lgm2yr)
   
   
   CTE_control %>% 
+    filter(Year > 2002)%>% # full years 
   ggplot() +
   geom_boxplot(aes(x = "", 
                    y = RL)) +
   xlab("R/L") +
-  ggtitle("Control plots of CTE, El Verde, Luquillo") +
+  ggtitle("Control plots of CTE, El Verde, Luquillo (2003-2019)") +
   adams_theme
   
 
