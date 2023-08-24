28aa712007e36762778417059d62ae8deba4ac65
adamhb <ahanburybrown@gmail.com>
2023-08-24 10:06:23 -0700

    cleaned up repo

--- Rscripts/compare_cwdtest_to_main_by_decade.R
+++
@@ -1,5 +0,0 @@
-library(tidyverse)
-
-
-bft <- read_csv("~/cloud/gdrive/postdoc/simulation_output/burn_frac_test.csv")
-fit <- read_csv("~/cloud/gdrive/postdoc/simulation_output/burn_frac_test.csv")
